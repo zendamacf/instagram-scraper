d64c812ecc3aeeb8800521ad5f3c059929891aa7
Zach Lang <zach.d.lang@gmail.com>
2019-09-04 23:09:22 +1200

    Moves URL to CLI parameter

--- __init__.py
+++ __init__.py
@@ -1,8 +1,24 @@
+import argparse
 import scraper
 
 
+def _get_parser():
+	parser = argparse.ArgumentParser(
+		description='Scrape data from a Instagram profile.'
+	)
+
+	parser.add_argument(
+		'url',
+		help='URL of the Instagram profile to scrape.'
+	)
+
+	return parser
+
+
 def main():
-	posts = scraper.profile_recent_posts('https://www.instagram.com/nine.inch.nails/?hl=en')
+	parser = _get_parser()
+	args = parser.parse_args()
+	posts = scraper.profile_recent_posts(args.url)
 
 	post_data = []
 	for p in posts:
